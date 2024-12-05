62319bac022ae9ef3c858f41e8d40ddd5225e151
danieski <danielesquivelgarcia@gmail.com>
2024-12-05 20:11:43 +0100

    3Arrows on the second bow

--- obj/Debug/net8.0/motor.AssemblyInfo.cs
+++ obj/Debug/net8.0/motor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("motor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdc7b6e0e68db3ed9411e5d7b27fe4e7797aff2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d674a3b74c5d501a67ec1d6aa8e50e1edd5e9b5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("motor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("motor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
